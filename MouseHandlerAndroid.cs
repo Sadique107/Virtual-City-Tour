7fc1d959d8d5b1997d0bd98d12770ac6877fdecb
Sadique Rahman <81058615+Sadique107@users.noreply.github.com>
2025-05-15 03:04:47 +0530

    Add files via upload

---
+++ MouseHandlerAndroid.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class MouseHandlerAndroid : MonoBehaviour
+{
+    public VariableJoystick variableJoystick;
+    public float horizontalSpeed = 1f;
+    
+    public float verticalSpeed = 1f;
+    private float xRotation = 0.0f;
+    private float yRotation = 0.0f;
+    private Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        float mouseX = variableJoystick.Horizontal * horizontalSpeed;
+        float mouseY = variableJoystick.Vertical * verticalSpeed;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        transform.eulerAngles = new Vector3(xRotation, yRotation, 0.0f);
+    }
+}
